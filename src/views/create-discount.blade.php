fb4478e2be99fdaaa8e25297bafda109fc10e021
abolfazl <abolfazlrastegar1996@gmail.com>
2022-05-21 12:30:51 -0700

    Update create-discount.blade.php

--- src/views/create-discount.blade.php
+++ src/views/create-discount.blade.php
@@ -1,5 +1,5 @@
 @if(config('discount.public.bootstrap'))
-    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.0.0/css/bootstrap.min.css" integrity="sha384-P4uhUIGk/q1gaD/NdgkBIl3a6QywJjlsFJFk7SPRdruoGddvRVSwv5qFnvZ73cpz" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('vendor/discount/css/bootstrap.min.css')}}">
 @endif
 <link rel="stylesheet" href="{{asset('vendor/discount/css/persianDatepicker-default.css')}}">
 <link rel="stylesheet" href="{{asset('vendor/discount/css/persianDatepicker-dark.css')}}">
@@ -188,10 +188,10 @@
     </div>
 </div>
 @if(config('discount.public.bootstrap'))
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="{{asset('vendor/discount/js/jquery-3.2.1.slim.min.js')}}"></script>
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
     <script src="{{asset('vendor/discount/js/sweetalert2@11.js')}}"></script>
-    <script src="https://cdn.rtlcss.com/bootstrap/v4.0.0/js/bootstrap.min.js" integrity="sha384-54+cucJ4QbVb99v8dcttx/0JRx4FHMmhOWi4W+xrXpKcsKQodCBwAvu3xxkZAwsH" crossorigin="anonymous"></script>
+    <script src="{{asset('vendor/discount/js/bootstrap.min.js')}}"></script>
 @endif
 <script src="{{asset('vendor/discount/js/persianDatepicker.min.js')}}"></script>
 <script src="{{asset('vendor/discount/js/discount.js')}}"></script>
