aa3bab8c1a6125aae2001f3766f91f6dc8db5646
abolfazl <abolfazlrastegar1996@gmail.com>
2022-05-22 17:26:46 -0700

    Update DiscountServiceProvider.php

--- src/Provider/DiscountServiceProvider.php
+++ src/Provider/DiscountServiceProvider.php
@@ -5,6 +5,7 @@ namespace Abolfazlrastegar\LaravelDiscount\Provider;
 use Abolfazlrastegar\LaravelDiscount\Components\CreateDiscount;
 use Abolfazlrastegar\LaravelDiscount\DiscountController;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class DiscountServiceProvider extends ServiceProvider
@@ -14,23 +15,32 @@ class DiscountServiceProvider extends ServiceProvider
         $this->app->bind('discount', function ($app) {
             return new DiscountController();
         });
+
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Discount', "Abolfazlrastegar\LaravelDiscount\Facades\Discount");
+
+        $this->registerRoutes();
     }
 
     public function boot()
     {
-        $this->registerMigration();
-        $this->registerConfig();
-        $this->registerView();
-        $this->registerPublic();
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->registerMigrations();
+        $this->registerConfigs();
+        $this->registerViews();
+        $this->registerPublics();
         $this->loadViewComponentsAs('Discount', [CreateDiscount::class]);
 
         Paginator::defaultView('discount::pagination');
         Paginator::defaultSimpleView('discount::pagination');
     }
 
+    protected function registerRoutes () {
+        Route::group(['middleware' => config('discount.middleware'), 'prefix' => config('discount.prefix')], function () {
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        });
+    }
 
-    protected function registerView () {
+    protected function registerViews () {
         $this->loadViewsFrom(__DIR__ . '../views', 'discount');
         $this->publishes([
             realpath(__DIR__ . '/../views') => base_path('resources/views/vendor/discount')
@@ -38,19 +48,19 @@ class DiscountServiceProvider extends ServiceProvider
     }
 
 
-    protected function registerPublic () {
+    protected function registerPublics () {
         $this->publishes([
             __DIR__ . '/../public' => public_path('vendor/discount'),
         ], 'public');
     }
 
-    protected function registerConfig () {
+    protected function registerConfigs () {
         $this->publishes([
             realpath(__DIR__ . '/../config/discount.php') =>  config_path('discount.php')
         ], 'config');
     }
 
-    protected function registerMigration () {
+    protected function registerMigrations () {
         $this->loadMigrationsFrom(__DIR__ . '/../Migrations');
         $this->publishes([
             realpath(__DIR__ . '/../Migrations') => database_path('migrations'),
