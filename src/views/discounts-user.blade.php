88e0a78ced0434d4c414baa8328d510d61967045
abolfazl <abolfazlrastegar1996@gmail.com>
2022-05-22 17:27:16 -0700

    Update discounts-user.blade.php

--- src/views/discounts-user.blade.php
+++ src/views/discounts-user.blade.php
@@ -1,89 +1,86 @@
 @section('title', 'users discount')
 @extends(config('discount.layouts'))
 @section('content')
-@extends('discount::layout')
-@section('content_discount')
-    <div class="container-fluid" style="direction: rtl">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">لیست کابران استفاده کردن از کد تخفیف</h4>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead class="thead-dark">
+<div class="container-fluid" style="direction: rtl">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">لیست کابران استفاده کردن از کد تخفیف</h4>
+                </div>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col" class="text-center">نام و نام خانوادگی</th>
+                            <th scope="col" class="text-center">کد تخفیف</th>
+                            <th scope="col" class="text-center">مبلغ کد</th>
+                            <th scope="col" class="text-center">بخش استفاده شده</th>
+                            <th scope="col" class="text-center">تاریخ</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($discount_user as $discount)
                             <tr>
-                                <th scope="col">#</th>
-                                <th scope="col" class="text-center">نام و نام خانوادگی</th>
-                                <th scope="col" class="text-center">کد تخفیف</th>
-                                <th scope="col" class="text-center">مبلغ کد</th>
-                                <th scope="col" class="text-center">بخش استفاده شده</th>
-                                <th scope="col" class="text-center">تاریخ</th>
+                                <th scope="row">{{$discount->id}}</th>
+                                <td class="text-warning text-center">{{$discount->user->name }} {{$discount->user->family }}</td>
+                                <td class="text-primary text-center">{{$discount->discount->code}}</td>
+                                <td class="text-center">{{number_format((int)$discount->discount->price)}} تومان </td>
+                                <td class="text-danger text-center">{{$discount->section_used}}</td>
+                                <td class="text-center">{{\Morilog\Jalali\CalendarUtils::strftime('Y-m-d', strtotime($discount->created_at))}}</td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($discount_user as $discount)
-                                <tr>
-                                    <th scope="row">{{$discount->id}}</th>
-                                    <td class="text-warning text-center">{{$discount->user->name }} {{$discount->user->family }}</td>
-                                    <td class="text-primary text-center">{{$discount->discount->code}}</td>
-                                    <td class="text-center">{{number_format((int)$discount->discount->price)}} تومان </td>
-                                    <td class="text-danger text-center">{{$discount->section_used}}</td>
-                                    <td class="text-center">{{\Morilog\Jalali\CalendarUtils::strftime('Y-m-d', strtotime($discount->created_at))}}</td>
-                                </tr>
-                                <div class="modal fade edit-{{$discount->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-lg">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title h4" id="myLargeModalLabel">کد تخفیف جدید</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">×</span>
-                                                </button>
-                                            </div>
-                                            <form autocomplete="off" id="edit-discount" method="POST" action="?type=edit&discountId=1">
-                                                <div class="modal-body">
-                                                    <div class="row">
-                                                        <div class="col-6">
-                                                            <label>کد تخفیف</label>
-                                                            <button type="button" class="btn btn-primary btn-sm"  onclick="createCodeDiscount('edit-code')">ایجاد کد</button>
-                                                            <input type="text" name="code" id="edit-code" value="{{$discount->code}}" class="form-control" placeholder="کد تخفیف" onkeyup="success(this)">
-                                                        </div>
-                                                        <div class="col-6">
-                                                            <label>تعداد استفاده</label>
-                                                            <input type="text" name="quantity" value="{{$discount->quantity}}" id="edit-quantity" class="form-control" placeholder="تعداد استفاده" onkeyup="success(this)">
-                                                        </div>
-                                                        <div class="col-12">
-                                                            <label>مبلغ تخفیف</label>
-                                                            <input type="text" name="price" id="edit-price" value="{{number_format((int)$discount->price)}}" class="form-control" placeholder="مبلغ تخفیف" onkeyup="success(this)">
-                                                        </div>
-                                                        <div class="col-6">
-                                                            <label>تاریخ شروع</label>
-                                                            <input type="text" class="form-control edit-1-start_date" value="{{\Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strftime($discount->start_date))}}" placeholder="تاریخ شروع" onclick="datepicker(['.edit-1-start_date', '#edit-1-start_date'])">
-                                                            <input type="hidden" name="end_date" id="edit-1-start_date">
-                                                        </div>
-                                                        <div class="col-6">
-                                                            <label>تاریخ پایان</label>
-                                                            <input type="text" class="form-control edit-1-end_date" value="{{\Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strftime($discount->end_date))}}" placeholder="تاریخ پایان" onclick="datepicker(['.edit-1-end_date', '#edit-1-end_date'])">
-                                                            <input type="hidden" name="end_date" id="edit-1-end_date">
-                                                        </div>
+                            <div class="modal fade edit-{{$discount->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title h4" id="myLargeModalLabel">کد تخفیف جدید</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <form autocomplete="off" id="edit-discount" method="POST" action="?type=edit&discountId=1">
+                                            <div class="modal-body">
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <label>کد تخفیف</label>
+                                                        <button type="button" class="btn btn-primary btn-sm"  onclick="createCodeDiscount('edit-code')">ایجاد کد</button>
+                                                        <input type="text" name="code" id="edit-code" value="{{$discount->code}}" class="form-control" placeholder="کد تخفیف" onkeyup="success(this)">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <label>تعداد استفاده</label>
+                                                        <input type="text" name="quantity" value="{{$discount->quantity}}" id="edit-quantity" class="form-control" placeholder="تعداد استفاده" onkeyup="success(this)">
+                                                    </div>
+                                                    <div class="col-12">
+                                                        <label>مبلغ تخفیف</label>
+                                                        <input type="text" name="price" id="edit-price" value="{{number_format((int)$discount->price)}}" class="form-control" placeholder="مبلغ تخفیف" onkeyup="success(this)">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <label>تاریخ شروع</label>
+                                                        <input type="text" class="form-control edit-1-start_date" value="{{\Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strftime($discount->start_date))}}" placeholder="تاریخ شروع" onclick="datepicker(['.edit-1-start_date', '#edit-1-start_date'])">
+                                                        <input type="hidden" name="end_date" id="edit-1-start_date">
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <label>تاریخ پایان</label>
+                                                        <input type="text" class="form-control edit-1-end_date" value="{{\Morilog\Jalali\CalendarUtils::strftime('Y/m/d', strftime($discount->end_date))}}" placeholder="تاریخ پایان" onclick="datepicker(['.edit-1-end_date', '#edit-1-end_date'])">
+                                                        <input type="hidden" name="end_date" id="edit-1-end_date">
                                                     </div>
                                                 </div>
-                                                <div class="modal-footer">
-                                                    <button type="submit" class="btn btn-primary">ذخیره</button>
-                                                </div>
-                                            </form>
-                                        </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="submit" class="btn btn-primary">ذخیره</button>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        {{$discount_user->links()}}
-                    </div>
+                            </div>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {{$discount_user->links()}}
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</div>
 @endsection
