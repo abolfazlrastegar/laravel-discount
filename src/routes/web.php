2a1d3370dec095519705ca957a657b4ddb8332ec
abolfazl <abolfazlrastegar1996@gmail.com>
2022-05-22 17:26:56 -0700

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use \Illuminate\Support\Facades\Route;
-Route::prefix(config('discount.url.prefix'))->namespace('\Abolfazlrastegar\LaravelDiscount')->group(function () {
+Route::group(['namespace' => '\Abolfazlrastegar\LaravelDiscount', 'middleware' => config('discount.middleware')], function () {
    Route::post('create', 'DiscountController@create');
    Route::post('edit-discount', 'DiscountController@edit');
    Route::get('remove/discount/{id}', 'DiscountController@removeDiscount');
    Route::get('discount/status', 'DiscountController@statusDiscount');
-   Route::get('users/discount/{discount_id}', 'DiscountController@getUserOneDiscount');
-   Route::get('discounts/user/{user_id}', 'DiscountController@getDiscountUsedUser');
+   Route::get('users/discount/{discount_id}', 'DiscountController@getUserOneDiscount')->name('users-discount');
+   Route::get('discounts/user/{user_id}', 'DiscountController@getDiscountUsedUser')->name('discounts-user');
 });
